048f7fb4faf839c0952476bde204efe476858dd0
prouget <prouget2000@yahoo.fr>
2019-04-18 14:48:34 +0200

    setup update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from cx_Freeze import setup, Executable
 
 setup(
     name = "ProdesMots",
-    version = "1.0.0",
+    version = "1.0.1",
     description = "recherche de mots par dictionnaire",
     executables = [Executable("Anagramme.py")],
 )
